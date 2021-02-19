14f60798a0d4ac690a7eef21784822d702c17ae0
David <timapple@mailfence.com>
2021-02-18 23:23:13 -0500

    Update run.py

--- part one/run.py
+++ part one/run.py
@@ -1,8 +1,9 @@
+#!/venv/bin/python
 from flask import Flask
-app = Flask(__name__)
-
 from pyowm import OWM
 from flask import request
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
@@ -15,15 +16,18 @@ def weather(country, city):
     weather_manager = owm.weather_manager()
     weather_at_place = weather_manager.weather_at_place(f'{country},{city}')
     temperature = weather_at_place.weather.temperature('celsius')
-    
+
     weather_details = {
         'temp_celsius': temperature['temp'],
         'temp_kelvin': temperature['temp'] + 273
     }
-    if (request.args.get('show_humidity')):
+
+    if (request.args.get('humidity')):
         weather_details['humidity'] = weather_at_place.weather.humidity
-    if (request.args.get('show_precipitation')):
-        weather_details['precipitation_last_hour'] = weather_at_place.weather.rain['1h']
+
+    if (request.args.get('detail')):
+        weather_details['detail'] = weather_at_place.weather.detailed_status
+
     return weather_details
 
 # Keep this at the bottom of app.py
